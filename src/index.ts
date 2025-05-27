00f96687da2bc9980187f0960926eb4c06419082
Tsimafei <xsetiqq@gmail.com>
2025-05-27 13:29:17 +0200

    add logic 2

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,10 @@ let todos: Todo[] = [];
 app.use(express.json());
 
 app.get("/todos", (req, res) => {
+  res.status(200).json(todos);
+});
+
+app.post("/todos", (req, res) => {
   const { title } = req.body;
   const newTodo: Todo = {
     id: Date.now(),
@@ -19,16 +23,25 @@ app.get("/todos", (req, res) => {
   res.status(201).send(newTodo);
 });
 
-app.post("/todos", (req, res) => {
-  res.send(req.body);
-});
-
 app.delete("/todos/:id", (req, res) => {
-  res.send("Hello World!");
+  const deleteId = Number(req.params.id);
+  todos = todos.filter((item) => item.id !== deleteId);
+  res.json(todos);
 });
 
 app.put("/todos/:id", (req, res) => {
-  res.send("Hello World!");
+  const putId = Number(req.params.id);
+
+  const putTodosIndex = todos.findIndex((item) => item.id === putId);
+
+  if (putTodosIndex === -1) {
+    res.status(404).json({ error: "Id is not found" });
+  } else {
+    todos[putTodosIndex] = req.body;
+    todos[putTodosIndex].id = putId;
+
+    res.status(200).send(todos[putTodosIndex]);
+  }
 });
 
 app.listen(port, () => {
